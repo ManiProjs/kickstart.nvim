aad00b69cf5118498ab9bb9371daa86b2bc5cb0f
Mani Arasteh <mani.arasteh92@gmail.com>
2026-06-23 20:02:26 +0330

    New plugins!

--- init.lua
+++ init.lua
@@ -1328,3 +1328,16 @@ vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
 
 vim.o.showtabline = 0
+
+vim.keymap.set("n", "<leader>qs", function()
+  require("persistence").load()
+end, { desc = "Restore Session" })
+
+vim.keymap.set("n", "<leader>ql", function()
+  require("persistence").load({ last = true })
+end, { desc = "Restore Last Session" })
+
+vim.keymap.set("n", "<leader>qd", function()
+  require("persistence").stop()
+end, { desc = "Don't Save Current Session" })
+
